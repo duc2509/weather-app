4170c181723956040678a159c4c8f57f42e52a65
duc2509 <brianng2599@gmail.com>
2020-10-03 18:47:54 +0700

    Updates

---
+++ precache-manifest.cc6607b31df70fd03b53d4fa63371edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cb76d1b346c7d5ef7e116ad801b01c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0b531579dda0b10a022",
+    "url": "/static/css/main.e9572f68.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b531579dda0b10a022",
+    "url": "/static/js/main.41a7f0c8.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "02bfc7bf7a582b9189193424a3ec4f60",
+    "url": "/static/media/mountains-1645078_1280.02bfc7bf.jpg"
+  }
+]);
